3d85aab8fadad2a504a0a691e981c2457f0958f5
Alexxboc <boccardi.alessandro@gmail.com>
2022-04-05 14:07:26 +0200

    change main js strucrure

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,80 +11,71 @@ Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 
 */
 
-
-
-// Creo una funzione che generi le celle
-function generateGrid(number_of_cells, selector, element_name, class_name) {
-    // Seleziono l'elemento contenitore nella DOM
-    const cellsElement = document.querySelector(selector)
-    // Creo una stringa vuota per evitare la concatenazione delle griglie
-    cellsElement.innerHTML = ''
-    // Genero il numero di celle con un ciclo for
-    for (let i = 1; i <= number_of_cells; i++) {
-         // Dichiaro una variabile dove creo l'elemento della DOM
-        const cell = document.createElement(element_name)
-        // Determino la grandezza delle celle
-        cell.style.width = `calc(100% / ${number_of_cells})`
-        // Creo una costante per stampare i numeri
-        const gridNumbers = i
-        // console.log(gridNumbers);
-        // Aggiungo la classe all'elemento 
-        cell.classList.add(class_name)
-        // Scrivo i numeri nelle caselle
-        cell.innerHTML = gridNumbers
-        // Appendo l'elemento nella DOM
-        cellsElement.append(cell)
-    }
-}
-        
-
-  // Creo una funzione che selezioni TUTTI gli elementi creati
-  function selectCells(selector) {
-    //   Dichiaro una variabile dove seleziono gli elementi
-    const cells = document.querySelectorAll(selector)
-    return cells
-  }
-
-// Creo una funzione per la classe active 
-function activateCell (selector, active_class) {
-    // Dichiaro una variabile per la selezione delle celle
-    const cells = selectCells(selector)
-    // Creo un ciclo for
-    for(let i = 0; i < cells.length; i++) {
-        // Dichiaro una variabile per le singole celle
-        const cell = cells[i]
-        // Creo un evento al click
-        cell.addEventListener('click', function() {
-            // console.log(this);
-            this.classList.add(active_class)
-        })
-    }
-}
-
-// Seleziono l'elemento nel DOM dal quale estrapolare il livello di difficoltà del gioco
-const button = document.getElementById(`button`);
-
-// Creo un evento al click
+// Seleziono il bottone nella DOM dal quale estrapolare il livello di difficoltà
+const button = document.getElementById('button');
+// console.log(button);
+// Creo un evento di ascolto dove il giocatore selezionerà il livello di difficoltà
 button.addEventListener('click', function(){
-    // Estrapolo il valore del livello di diofficoltà
-    let difficulty = document.getElementById('level_difficulty').value
-    
-    // Creo le condizioni per i 3 livelli di difficoltà
-    if(difficulty === 'Difficoltà 1') {
-        generateGrid(100, '.cells', 'div', 'cell')
-        activateCell('.cell', 'selected')
-
-
-    } else if (difficulty === 'Difficoltà 2') {
-        generateGrid(81, '.cells', 'div', 'cell')
-        activateCell('.cell', 'selected')
-
-    } else {
-        generateGrid(49, '.cells', 'div', 'cell')
-        activateCell('.cell', 'selected')
-    }
+    // console.log('click');
+    // Estrapolo il livello scelto dal giocatore
+    const level = document.getElementById('level').value;
+    // console.log(level);
+    // Dichiaro due variabili per il numero delle celle e il numero delle colonne
+    let number_of_cells, number_of_cols;
+    // creo le condizioni per la disposizione della griglia in ogni livello
+    switch (level) {
+        case 'easy':
+          number_of_cells = 100;
+          number_of_cols = 10; 
+          break;
+        case 'medium':
+          number_of_cells = 81;
+          number_of_cols = 9;
+          break;
+        case 'hard':
+          number_of_cells = 49;
+          number_of_cols = 7;
+          break;
+      }
+    // console.log(number_of_cells, number_of_cols);
+    generateGrid(number_of_cells, number_of_cols)
+    addActiveStyle('.cell','selected')
 })
 
+// Creo una funzione che generi la griglia
+function generateGrid (number_of_cells, number_of_cols) {
+    // Dichiaro una variabile che selezioni l'area di gioco
+    const gameArea = document.querySelector('.cells')
+    // Pulisco l'area di gioco
+    gameArea.innerHTML = ''
+    for(let i = 1; i <= number_of_cells; i++){
+        const cell = document.createElement('div')
+        // Appendo alla cella il numero progressivo
+        cell.append(i)
+        cell.classList.add('cell')
+        // Dimensiono la larghezza della cella
+        cell.style.width = `calc(100% / ${number_of_cols})`
+        // Appendo la cella all'area di gioco
+        gameArea.append(cell)
+    }
+}
+
+// Creo una funzione per cambiare lo stile delle caselle al click
+function addActiveStyle (selector, style_class) {
+    // Seleziono tutti gli elememnti della DOM da modificare
+    const cells = document.querySelectorAll(selector);
+    // Creo un ciclo for per tutti gli elementi
+    for (let i = 0; i < cells.length; i++) {
+        const cellsElement = cells[i];
+        // Creo un evento al click
+        cellsElement.addEventListener('click', function () {
+            //console.log(this);
+            // Coloro la casella 
+            this.classList.add(style_class);
+          });
+        
+    }
+}
 
 
 
